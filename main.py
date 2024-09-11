1512dd7f77b41c8a22b8547fc2e6c03c0c79366f
Gustavo Malisse <gostavomalisse@gmail.com>
2024-09-11 16:22:27 -0300

    Added rectangles and started implementing collision

--- main.py
+++ main.py
@@ -12,12 +12,17 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black')
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
 snail_x_pos = 600
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom=(snail_x_pos, 300))
+
+
+player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(midbottom=(80, 300))
 
 while True:
 
@@ -31,11 +36,15 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
+    screen.blit(player_surface, player_rect)
 
-    snail_x_pos -= 4
-    if snail_x_pos < -100:
-        snail_x_pos = 800
-    screen.blit(snail_surface, (snail_x_pos, 270))
+    if player_rect.colliderect(snail_rect):
+        print('Collision')
+
+    snail_rect.x -= 4
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+    screen.blit(snail_surface, snail_rect)
     pygame.display.update()
     clock.tick(60)
 
