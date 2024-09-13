c6cafb51836fba16077c234110f56472da048c47
Gustavo Malisse <gostavomalisse@gmail.com>
2024-09-12 21:14:58 -0300

    Added gravity, jump and ground

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render('My game', False, 'Black')
+text_surface = test_font.render('My game', False, (64, 64, 64))
 text_rect = text_surface.get_rect(center=(400, 50))
 
 snail_x_pos = 600
@@ -24,6 +24,7 @@ snail_rect = snail_surface.get_rect(midbottom=(snail_x_pos, 300))
 
 player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surface.get_rect(midbottom=(80, 300))
+player_gravity = 0
 
 while True:
 
@@ -33,26 +34,32 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        # if event.type == pygame.MOUSEMOTION:
-        #     if player_rect.collidepoint(event.pos):
-        #         print('collision')
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                player_gravity = -20
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
+    pygame.draw.rect(screen, '#c0e8ec', text_rect)
+    pygame.draw.rect(screen, '#c0e8ec', text_rect, 10)
     screen.blit(text_surface, text_rect)
-    screen.blit(player_surface, player_rect)
-
-    # if player_rect.colliderect(snail_rect):
-    #     print('Collision')
-
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint(mouse_pos):
-    #     print(pygame.mouse.get_pressed())
 
     snail_rect.x -= 4
     if snail_rect.right <= 0:
         snail_rect.left = 800
     screen.blit(snail_surface, snail_rect)
+
+    player_gravity += 1
+    player_rect.y += player_gravity
+    keys = pygame.key.get_pressed()
+    if player_rect.bottom >= 300 and keys[pygame.K_SPACE]:
+        player_gravity = -20
+
+    if player_rect.bottom > 300:
+        player_rect.bottom = 300
+
+    screen.blit(player_surface, player_rect)
+
     pygame.display.update()
     clock.tick(60)
 
